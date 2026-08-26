b39a0db72102050bdfbe83a72035ade303110a3d
Bence Safranek <safranek.bence57@gmail.com>
2026-08-26 21:28:05 +0200

    add slotmachine symbols and start spin method logic

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SlotMachine {
+    private List<String> symbols;
+
+    public SlotMachine() {
+        this.symbols = new ArrayList<>();
+        this.symbols.add("🍒");
+        this.symbols.add("🍋");
+        this.symbols.add("🔔");
+        this.symbols.add("⭐");
+        this.symbols.add("💎");
+    }
+
+    public void spin(){
+        int randomNumOne = (int) (Math.random() * this.symbols.size());
+        int randomNumTwo = (int) (Math.random() * this.symbols.size());
+        int randomNumThree = (int) (Math.random() * this.symbols.size());
+        String firstSymbol = symbols.get(randomNumOne);
+        String secondSymbol = symbols.get(randomNumTwo);
+        String thirdSymbol = symbols.get(randomNumThree);
+        if (firstSymbol.equals(secondSymbol) && thirdSymbol.equals(secondSymbol)){
+
+        }
+        System.out.println("|" +  firstSymbol + "|" + secondSymbol + "|" + thirdSymbol + "|");
+    }
 }
