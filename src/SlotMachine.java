aac92f8ed9fe23fa109a5733a7d9a03116421316
Bence Safranek <safranek.bence57@gmail.com>
2026-08-27 12:39:49 +0200

    start to continue spin method logic -- needs finishing

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -15,16 +15,34 @@ public class SlotMachine {
         this.symbols.add("💎");
     }
 
-    public void spin(){
+
+    public int spin(){
         int randomNumOne = (int) (Math.random() * this.symbols.size());
         int randomNumTwo = (int) (Math.random() * this.symbols.size());
         int randomNumThree = (int) (Math.random() * this.symbols.size());
         String firstSymbol = symbols.get(randomNumOne);
         String secondSymbol = symbols.get(randomNumTwo);
         String thirdSymbol = symbols.get(randomNumThree);
-        if (firstSymbol.equals(secondSymbol) && thirdSymbol.equals(secondSymbol)){
-
+        boolean twoEqual = firstSymbol.equals(secondSymbol) || thirdSymbol.equals(secondSymbol) || firstSymbol.equals(thirdSymbol);
+        boolean threeEqual = firstSymbol.equals(secondSymbol) && thirdSymbol.equals(secondSymbol);
+        if(firstSymbol.equals(secondSymbol) && thirdSymbol.equals(secondSymbol) && firstSymbol.equals("🍒") || firstSymbol.equals("🍋")){
+            if (threeEqual){
+                return 3;
+            }
+            else if (twoEqual){
+                return 2;
+            }
+            else return 0;
+        } else if (firstSymbol.equals(secondSymbol) && thirdSymbol.equals(secondSymbol) && firstSymbol.equals("🔔") || firstSymbol.equals("⭐")){
+            if (threeEqual){
+                return 4;
+            }
+            else if (twoEqual){
+                return 2;
+            }
+            else return 0;
         }
-        System.out.println("|" +  firstSymbol + "|" + secondSymbol + "|" + thirdSymbol + "|");
+
+        //System.out.println("|" +  firstSymbol + "|" + secondSymbol + "|" + thirdSymbol + "|");
     }
 }
